e816951a3081cf00cec89199fdb6f22b1f856b84
t_sugiyamads <t_sugiyamads@quest-com.co.jp>
2017-05-07 13:47:25 +0900

    リスト表示の開閉を修正

--- ReminderClient/js/custom_items.js
+++ ReminderClient/js/custom_items.js
@@ -3,8 +3,15 @@ $(function () {
     var menuBtn = $('#list_open');
     var content = $('#content');
 
+    var isOpen = $('#content').hasClass('list_open');
     menuBtn.on('click', function() {
-      $('#content').toggleClass('list_open');
-      $('#list').toggleClass('close')
+      if (isOpen) {
+        $('#content').removeClass('list_open');
+        $('#list').removeClass('close');
+      } else {
+        $('#content').addClass('list_open');
+        $('#list').addClass('close');
+      }
+      isOpen = !isOpen;
     });
 });
